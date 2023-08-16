aec2591a6cbdaf4b15ef8c362093c457ab8bd3c9
Marica Pasquali <marica.pasquali@studio.unibo.it>
2023-08-16 14:05:31 +0200

    add comment to managers of discovery server

--- discovery/src/managers/peers/index.ts
+++ discovery/src/managers/peers/index.ts
@@ -1,15 +1,41 @@
+/**
+ * It represents a generic manager of peers.
+ */
 interface IPeersManager {
+  /**
+   * List of registered peer on discovery server.
+   */
   peers: Peer[];
 
+  /**
+   * Add a peer on discovery server.
+   * @param peer peer information to add.
+   */
   addPeer(peer: Peer): void;
 
+  /**
+   * Update status of a given peer.
+   * @param id identifier of peer to update.
+   * @param status new status of peer.
+   */
   updatePeer(id: string, status: string): void;
 
+  /**
+   * Remove a given peer from discovery server.
+   * @param id identifier of peer to remove.
+   */
   removePeer(id: string): void;
 
+  /**
+   * Find a peer given an identifier.
+   * @param id identifier of peer to find.
+   */
   findPeer(id: string): Peer | undefined;
 }
 
+/**
+ * Implementation and instantiation of _IPeersManager_.
+ */
 const PeersManager: IPeersManager = new (class implements IPeersManager {
   private readonly _peers: Peer[] = [];
 
